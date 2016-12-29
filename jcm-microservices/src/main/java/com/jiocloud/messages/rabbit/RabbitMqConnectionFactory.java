53bad216fcda71244633a7f7deb4bca0b9eead82
Abhinava Srivastava <abhinavasrivastava@Administrators-MacBook-Pro.local>
2016-12-29 20:53:01 +0530

    rabbitmq connection url added

--- jcm-microservices/src/main/java/com/jiocloud/messages/rabbit/RabbitMqConnectionFactory.java
+++ jcm-microservices/src/main/java/com/jiocloud/messages/rabbit/RabbitMqConnectionFactory.java
@@ -1,12 +1,14 @@
 package com.jiocloud.messages.rabbit;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.util.ResourceBundle;
 import java.util.concurrent.TimeoutException;
 
 import org.springframework.stereotype.Component;
 
-import com.rabbitmq.client.Address;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -19,13 +21,15 @@ public class RabbitMqConnectionFactory {
 	Channel channel;
 	ResourceBundle rb = ResourceBundle.getBundle("jcm-app");
 	
-	public RabbitMqConnectionFactory() throws IOException, TimeoutException{
+	public RabbitMqConnectionFactory() throws IOException, TimeoutException, KeyManagementException, NoSuchAlgorithmException, URISyntaxException{
 		factory = new ConnectionFactory();
-        //factory.setHost("localhost");
+		factory.setUri("amqp://test:test@172.24.1.36");
+		//factory.setHost("localhost");
 		String[] hosts = rb.getString("rabbitmq.servers").split(",");
-		Address[] addresses = {new Address(hosts[0], 5672), new
-				Address(hosts[1], 5672)};
-		connection = factory.newConnection(addresses);
+//		Address[] addresses = {new Address(hosts[0], 5672), new
+//				Address(hosts[1], 5672)};
+//		connection = factory.newConnection(addresses);
+		connection = factory.newConnection();
 	    channel = connection.createChannel();
         channel.exchangeDeclare("textmessagesexchange", "direct");
         channel.queueBind("textmessages", "textmessagesexchange", "textmessagekey");
