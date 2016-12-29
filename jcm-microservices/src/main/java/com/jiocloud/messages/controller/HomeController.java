cf0699767f414ef99609f345c349e4a91d952e9f
Abhinava Srivastava <abhinavasrivastava@Administrators-MacBook-Pro.local>
2016-12-29 11:54:18 +0530

    service - embedded container

---
+++ jcm-microservices/src/main/java/com/jiocloud/messages/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.jiocloud.messages.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String hello(){
+		return "hello";
+	}
+}
