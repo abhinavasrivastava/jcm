3e4575a6333bd80078e8bc79841f6ace298ca57a
Abhinava Srivastava <abhinava.srivastava1@gmail.com>
2017-01-06 16:36:47 +0530

    protocol version 3

--- jcm-microservices/src/main/java/com/jiocloud/messages/dao/CassandraConnector.java
+++ jcm-microservices/src/main/java/com/jiocloud/messages/dao/CassandraConnector.java
@@ -35,9 +35,9 @@ public class CassandraConnector
 	   poolingOptions
 	     // .setConnectionsPerHost(HostDistance.LOCAL,  4, 10)
 	      //.setConnectionsPerHost(HostDistance.REMOTE, 2, 4);
-	   .setCoreConnectionsPerHost(HostDistance.LOCAL,  1)
+	   .setCoreConnectionsPerHost(HostDistance.LOCAL,  4)
 	   .setCoreConnectionsPerHost(HostDistance.REMOTE,  1)
-	      .setMaxRequestsPerConnection(HostDistance.LOCAL, 327680)
+	      .setMaxRequestsPerConnection(HostDistance.LOCAL, 32768)
 	      .setMaxRequestsPerConnection(HostDistance.REMOTE, 2000);
       this.cluster = Cluster.builder().addContactPoints(node.split(","))
     		  .withPort(port)
