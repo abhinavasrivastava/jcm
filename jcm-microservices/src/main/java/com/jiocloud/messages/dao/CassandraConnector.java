d1347c8a34fe87751bab29f3dafad1a0da5359e4
Abhinava Srivastava <abhinava.srivastava1@gmail.com>
2017-01-08 13:19:19 +0530

    protocol version 3

--- jcm-microservices/src/main/java/com/jiocloud/messages/dao/CassandraConnector.java
+++ jcm-microservices/src/main/java/com/jiocloud/messages/dao/CassandraConnector.java
@@ -2,6 +2,10 @@ package com.jiocloud.messages.dao;
 
 import static java.lang.System.out;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.HostDistance;
@@ -9,7 +13,7 @@ import com.datastax.driver.core.Metadata;
 import com.datastax.driver.core.PoolingOptions;
 import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.Session;
-import com.datastax.driver.core.policies.RoundRobinPolicy;
+import com.datastax.driver.core.policies.LoadBalancingPolicy;
 /**
  * Class used for connecting to Cassandra database.
  */
@@ -33,7 +37,7 @@ public class CassandraConnector
 
 	   PoolingOptions poolingOptions = new PoolingOptions();
 	   poolingOptions
-	     .setConnectionsPerHost(HostDistance.LOCAL,  2, 2)
+	     .setConnectionsPerHost(HostDistance.LOCAL,  1, 1)
 	      //.setConnectionsPerHost(HostDistance.REMOTE, 2, 4);
 	   //.setCoreConnectionsPerHost(HostDistance.LOCAL,  2)
 	   //.setCoreConnectionsPerHost(HostDistance.REMOTE,  1)
@@ -56,6 +60,35 @@ public class CassandraConnector
             host.getDatacenter(), host.getAddress(), host.getRack());
       }
       session = cluster.connect(keyspace);
+      
+      
+      
+      final LoadBalancingPolicy loadBalancingPolicy =
+    		    cluster.getConfiguration().getPolicies().getLoadBalancingPolicy();
+    		final PoolingOptions po =
+    		cluster.getConfiguration().getPoolingOptions();
+
+    		ScheduledExecutorService scheduled =
+    		Executors.newScheduledThreadPool(1);
+    		scheduled.scheduleAtFixedRate(new Runnable() {
+    		    @Override
+    		    public void run() {
+    		        Session.State state = session.getState();
+    		        for (Host host : state.getConnectedHosts()) {
+    		            HostDistance distance = loadBalancingPolicy.distance(host);
+    		            int connections = state.getOpenConnections(host);
+    		            int inFlightQueries = state.getInFlightQueries(host);
+//    		            System.out.printf("%s connections=%d, current load=%d, max
+//    		load=%d%n",
+//    		                host, connections, inFlightQueries,
+//    		                connections *
+//    		poolingOptions.getMaxRequestsPerConnection(distance));
+    		            
+    		           System.out.println(host + "- connections=" + connections + ", current load=" + inFlightQueries + ", max load=" + connections *
+    		po.getMaxRequestsPerConnection(distance));
+    		        }
+    		    }
+    		}, 5, 5, TimeUnit.SECONDS);
      
    }
    /**
